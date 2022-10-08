df42ef800897b3ed509126b7d36e60fa3eb0b50b
justinpraveen1417 <praveenc@thetopclassentertainment.com>
2022-10-08 10:37:13 +0530

    jg

---
+++ index.js
@@ -0,0 +1,23 @@
+// Require the framework and instantiate it
+const fastify = require('fastify')({ logger: true })
+const cors = require("@fastify/cors")
+
+// Declare a route
+fastify.get('/', async (request, reply) => {
+  return { hello: "sachin elone musk " }
+})
+
+fastify.register(cors,{
+    origin : "*"
+})
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen({ port: 8000 })
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start() 
